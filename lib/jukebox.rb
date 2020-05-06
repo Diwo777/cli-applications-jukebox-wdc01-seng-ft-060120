52a43a3fe70bde028714576c5e26d9fe74e20ac1
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 19:39:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip 
   songs.each_with_index do |song,number|
-    binding.pry
+  
     number += 1 
     if input.to_i == number || input == song 
       puts "Playing #{song}"
