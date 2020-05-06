8e5249cb446ae164eb4474f571f56c241107b429
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 19:26:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,47 @@
-# Add your code here
+require 'pry'
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip 
+  
+end 
+
+def list (songs)
+  songs.each_with_index do |song, index|
+  index += 1 
+  puts "#{index} #{song}"
+end
+end 
+
+def exit_jukebox 
+end 
+
+def run 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+
+# puts "Enter your name:"
+# users_name = gets.strip 
+
+# puts say_hello(users_name)
