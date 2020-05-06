423b15416c09755ceda5e471678d47d8459b1702
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 20:18:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.downcase.chomp 
-  
+  binding.pry
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
     elsif songs.include?(user_response)
