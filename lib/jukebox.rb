516fe4f4b9afdd59d8b596a2942bd21a4d1ef26b
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 20:39:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,8 +32,8 @@ def exit_jukebox
 end 
 
 def run(my_songs)
-  help()
-  input = prompt()
+  puts "Please enter a command:"
+  input = gets.strip
   
   while input != "exit"
     if input == "list"
