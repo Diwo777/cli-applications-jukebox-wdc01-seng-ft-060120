b21cd20033e151b29a088785dfcd681212413a7f
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 20:00:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,7 +15,7 @@ def play(songs)
     number += 1 
     if input.to_i == number || input == song
       puts "Playing #{song}"
-    else input.to_i != number || input != song
+    elsif input.to_i != number || input != song
       return "Invalid input, please try again"
  
  end 
